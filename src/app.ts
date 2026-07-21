df8dce7d82668eef7a9ae6740008db34de710c4d
nazmulhossenshanto <https://github.com/nazmulhossenshanto>
2026-07-21 11:12:27 +0600

    created user and profile tables

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,6 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-
 app.use(cors({origin: config.app_url , credentials: true}));
 
 app.get('/', (req: Request, res: Response) => {
--- src/server.ts
+++ src/server.ts
@@ -16,5 +16,4 @@ async function main(){
     }
 }
 
-
 main();
